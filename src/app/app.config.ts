95cca4735116353fbec716ade374cd06b35c2a4a
satheeshy07cs101@gmail.com <satheeshy07cs101@gmail.com>
2024-09-27 22:56:23 +0100

    resume page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
+import { provideAnimations} from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+
+import { CommonModule, NgIf } from '@angular/common';
+import { Component , Renderer2} from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import {AccordionModule} from 'ngx-bootstrap/accordion';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, CommonModule, NgIf],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
 
-  constructor(private titleService: Title ){
+  isWorkExperienceOpen: boolean = false;
+  isEducationOpen:boolean = false;
+  isCertificationOpen:boolean = false;
+  isSkillsOpen:boolean = false;
+  
+
+
+  constructor(private titleService: Title, private renderer: Renderer2 ){
     this.titleService.setTitle('Satheesh Vykuntapu- Resume');
+  }
+
+  DownloadFile(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'Resume.pdf');
+    link.setAttribute('download', 'Resume.pdf' );
+    link.click();
+    link.remove();
   }
 }
